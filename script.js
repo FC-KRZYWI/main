4ed7a6956437b89931ad65b13b50c5690fad92c7
FC-KRZYWI <148591869+FC-KRZYWI@users.noreply.github.com>
2023-10-26 12:32:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+function checkData(x, y) {
+  fetch('')
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+       const users = data.users;
 
+       for (let user in users) {
+         if (user[0] == x && user[1] == y) {
+           return ('can');
+         };
+       };
+
+      return ('cant');
+    })
+  .catch(error => {
+    console.error(error);
+  }
+};
+
+document.querySelector('.login').addEventListener('click',() => {
+  let checkIf = checkData(document.querySelector('.text').value, document.querySelector('.password.');
+
+  switch(checkIf) {
+    case 'can':
+      document.write('you are now logged');
+      break;
+    case 'cant':
+      document.write('you are not logged');
+      break;
+  };
+});
